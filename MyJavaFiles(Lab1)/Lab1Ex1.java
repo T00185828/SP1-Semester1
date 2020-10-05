70c60a65869ecf524e83e41798c0483bcadda2f5
T00185828 <Corey.Dennison@students.ittralee.ie>
2020-10-05 12:20:55 +0100

    Uploaded Lab 1 files
    Uploaded Lab 1 files of Structured Programming 1 module (Completed 5/10/2020)

---
+++ MyJavaFiles(Lab1)/Lab1Ex1.java
@@ -0,0 +1,13 @@
+//Lab1Ex1.java
+// Corey Dennison T00185828
+// This is a simple program that outputs text with println
+
+
+public class Lab1Ex1 {
+
+    public static void main (String[] args) {
+    
+   		System.out.println("My name is Paul and I live in Cork.");
+    
+    }
+}
---
+++ MyJavaFiles(Lab1)/Lab1Ex2.java
@@ -0,0 +1,12 @@
+//Lab1Ex2.java
+// Corey Dennison T00185828
+/* This program outputs a quote from Yoda using \", \n, and \t */
+
+public class Lab1Ex2 {
+	public static void main(String[] args){
+		
+		System.out.println("\"Fear is the path to the dark side \n Fear leads to anger.\nAnger leads to hate.\nHate leads to suffering\".\n \tMater Yoda"); 
+					
+	}
+	
+}
