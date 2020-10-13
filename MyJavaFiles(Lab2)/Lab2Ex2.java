1c6fd3733de1a1e537024ab96ef37ee61436b614
T00185828 <Corey.Dennison@students.ittralee.ie>
2020-10-13 15:29:04 +0100

    Update Lab2Ex2.java

--- MyJavaFiles(Lab2)/Lab2Ex2.java
+++ MyJavaFiles(Lab2)/Lab2Ex2.java
@@ -1,18 +1,20 @@
 //Lab2Ex2
 //Corey Dennison T00185828
-/*This program uses string variables to store information
+/*This program uses variables to store information
  *on a book and prints the data in a list format*/
  
  public class Lab2Ex2 {
  	public static void main(String[] args){
  		
- 		String title, author, pages, year, price;
+ 		String title, author;
+ 		int pages, year;
+ 		double price;
  		
  		title = "Harry Potter and the Prisoner of Azkaban";
  		author = "J.K Rowling";
- 		pages = "317";
- 		year = "1999";
- 		price = "35.5";
+ 		pages = 317;
+ 		year = 1999;
+ 		price = 35.50;
  		
  		System.out.println("Title: " + title + "\nAuthor: " + author + "\nNumber of Pages: " 
  			+ pages + "\nYear Published: " + year + "\nPrice: EUR" + price);
